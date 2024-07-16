c6771a8c3e811ceaea559796675b05a014ad6fa9
LASM24 <luisalbertosanabriamendez@gmail.com>
2024-07-16 17:18:44 -0500

    bd

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://db_admin:35ue2iM6UHTdfp97jv8pihWoKBLeTtsK@dpg-cq4vhqdds78s73cq96ig-a.oregon-postgres.render.com/events_web_app"
+SQLALCHEMY_DATABASE_URL = "postgresql://db_admin:8h6erWP1h59T5pwtWnZhn4sekU9i56ID@dpg-cqbf153v2p9s73eqfeb0-a/db_events_app"
 
 if "sqlite" in SQLALCHEMY_DATABASE_URL:
     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
