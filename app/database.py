6dfff5b1bb7c8626c05dbf630b185ed8dfb80a1f
LASM24 <luisalbertosanabriamendez@gmail.com>
2024-09-19 19:44:47 -0500

    fix

--- app/database.py
+++ app/database.py
@@ -1,9 +1,8 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://event_bd_user:2sr65I5x8PmaN6Mq0zXiUSKA6UfNpx7N@dpg-crmc8ci3esus73fqs730-a.oregon-postgres.render.com/event_bd"
 
 
 if "sqlite" in SQLALCHEMY_DATABASE_URL:
